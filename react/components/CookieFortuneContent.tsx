97d357514ff47d604272dbb358f00ce7053767b6
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2025-03-29 23:41:11 -0500

    Agrega estilos CSS para el componente CookieFortuneContent y mejora la estructura del componente para mostrar las frases de la galleta de la fortuna en una tabla. Se implementa manejo de errores y un estado de carga.

--- react/components/CookieFortuneContent.tsx
+++ react/components/CookieFortuneContent.tsx
@@ -1,25 +1,56 @@
 import React, { useEffect, useState } from 'react'
 
 import { getRandomFortune, ICookieFortuneResponse } from '../cookieFortune'
+import styles from './CookieFortuneContent.styles.css'
 
 const CookieFortuneContent = () => {
+  const [fortunes, setFortunes] = useState<ICookieFortuneResponse | null>(null)
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
-    const fetchFortune = async () => {
+    const fetchFortunes = async () => {
       try {
-        const response: ICookieFortuneResponse = await getRandomFortune()
+        const response = await getRandomFortune()
+
+        // console.log('Respuesta de getRandomFortune:', response);
+        setFortunes(response)
       } catch (err) {
-        setError('Error al obtener la frase de la fortuna')
+        setError('Error al obtener las frases de la fortuna')
       }
     }
 
-    fetchFortune()
+    fetchFortunes()
   }, [])
+  // console.log(fortunes);
 
   if (error) return <p>Error: {error}</p>
 
-  return <div>CookieFortuneContent</div>
+  return (
+    <div className={styles.CookieFortuneContent__mainContainer}>
+      <h2>Frases de la Galleta de la Fortuna</h2>
+      {fortunes ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Frase</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(fortunes) &&
+              fortunes.map(
+                (fortune: { CookieFortune: string }, index: number) => (
+                  <tr key={index}>
+                    <td>{fortune.CookieFortune}</td>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+      ) : (
+        <p>Cargando...</p>
+      )}
+    </div>
+  )
 }
 
 export default CookieFortuneContent
